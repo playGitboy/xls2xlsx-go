a1f63a244854a6b85db733cc9c2a7f9c6de92e40
playGitboy <72111157+playGitboy@users.noreply.github.com>
2023-03-21 11:24:39 +0800

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,8 @@
+module xls2xlsx
+
+go 1.14
+
+require (
+	github.com/shakinm/xlsReader v0.9.8
+	github.com/xuri/excelize/v2 v2.7.0
+)
---
+++ xls2xlsx.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	xls "github.com/shakinm/xlsReader/xls"
+	"github.com/xuri/excelize/v2"
+)
+
+func main() {
+	fmt.Print("---------------------------------------------------\n批量转换xls文件为xlsx（不依赖本机Office/WPS）\n放到xls文件所在目录，双击执行即可\n\n也可以命令行执行：\nxls2xlsx <xls文件所在目录>  \n---------------------------------------------------\n")
+	var xlsfilepath string
+	if len(os.Args) == 2 {
+		xlsfilepath = os.Args[1]
+	} else {
+		xlsfilepath, _ = filepath.Abs(filepath.Dir(os.Args[0]))
+	}
+
+	xlsName := regexp.MustCompile(`(?i)(\.xls)$`)
+	if xlsName.MatchString(xlsfilepath) {
+		xls2xlsx(xlsfilepath)
+	} else {
+		os.Chdir(xlsfilepath)
+
+		// 遍历目录
+		fss := curpathfiles(xlsfilepath)
+		for _, xlsfile := range *fss {
+			xls2xlsx(xlsfile)
+		}
+	}
+	fmt.Println("> 文件已全部转换完成！")
+	fmt.Scanln()
+}
+
+func curpathfiles(pathname string) *[]string {
+	rd, err := ioutil.ReadDir(pathname)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var filenames []string
+	var validID = regexp.MustCompile(`(?i)(\.xls)$`)
+
+	for _, fi := range rd {
+		if validID.MatchString(fi.Name()) {
+			filenames = append(filenames, fi.Name())
+		}
+	}
+	return &filenames
+}
+
+func xls2xlsx(xlsfile string) {
+	wb1, _ := xls.OpenFile(xlsfile)
+	newName := strings.TrimSuffix(xlsfile, filepath.Ext(xlsfile)) + ".xlsx"
+
+	f2 := excelize.NewFile()
+	for i := 0; i < wb1.GetNumberSheets(); i++ {
+		ws1, _ := wb1.GetSheet(i)
+		f2.NewSheet(ws1.GetName())
+		f2.DeleteSheet("Sheet1") //NewFile默认新建的Sheet1(必须填写新Sheet后才生效)
+		maxrows := ws1.GetNumberRows()
+		for r := 0; r < maxrows; r++ {
+			r1, _ := ws1.GetRow(r)
+			cols := r1.GetCols()
+			for c, cs := range cols {
+				ax, _ := excelize.CoordinatesToCellName(c+1, r+1)
+				f2.SetCellValue(ws1.GetName(), ax, cs.GetString())
+			}
+		}
+	}
+	fmt.Printf("> %q 转换完成，正在保存...\n", newName)
+	f2.SaveAs(newName)
+	f2.Close()
+}
